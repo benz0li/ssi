1d6e65f8081ef32bbf6db2731f332ef63fb2c340
Olivier Benz <olivier.benz@b-data.ch>
2023-08-12 08:01:32 +0200

    First draft

---
+++ scripts/start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copyright (c) 2023 b-data GmbH.
+# Distributed under the terms of the MIT License.
+
+set -e
+
+# Test if PREFIX location is whithin limits
+if [[ ! "$PREFIX" == "/usr/local" && ! "$PREFIX" =~ ^"/opt" ]]; then
+  echo "ERROR:  PREFIX set to '$PREFIX'. Must either be '/usr/local' or within '/opt'."
+  exit 1
+fi
+
+# Test if there is a bin folder at PREFIX
+if [[ ! -d "/tmp$PREFIX/bin" ]]; then
+  echo "ERROR:  There is no 'bin' folder in '$PREFIX'."
+  echo "INFO:   Execute 'mkdir -p $PREFIX/bin'" to create one.
+  exit 1
+fi
+
+if [[ "$MODE" == "install" ]]; then
+  # Install Cabal
+  cabal update
+  cabal install "cabal-install-$CABAL_VERSION_BUILD"
+  echo "Copying 'cabal' to '$(which cabal)'"
+  cp -aL /root/.cabal/bin/cabal "$(which cabal)"
+
+  # Download and extract source code
+  cd /tmp
+  curl -sSL https://github.com/commercialhaskell/stack/archive/refs/tags/v"$STACK_VERSION_BUILD".tar.gz \
+    -o stack.tar.gz
+  tar -xzf stack.tar.gz
+  cd stack-*
+
+  # Modify config
+  sed -i /stack/d cabal.config
+
+  # Build and install
+  cabal build \
+    --allow-older \
+    --enable-executable-static \
+    --ghc-options='-split-sections -optl=-pthread'
+
+  strip "$(find dist-newstyle -name stack -type f)"
+
+  cp -aL "$(find dist-newstyle -name stack -type f)" "/tmp$PREFIX/bin"
+  echo "INFO:   Stack v$STACK_VERSION_BUILD installed in '$PREFIX/bin'."
+elif [[ "$MODE" == "uninstall" ]]; then
+  if [[ -f "/tmp$PREFIX/bin/stack" ]]; then
+    rm -f "/tmp$PREFIX/bin/stack"
+    echo "INFO:   Stack uninstalled from '$PREFIX/bin'."
+  else
+    echo "ERROR:  Stack not found in '$PREFIX/bin'!"
+    exit 1
+  fi
+else
+  echo "ERROR:  Execution mode '$MODE' not supported!"
+  exit 1
+fi
